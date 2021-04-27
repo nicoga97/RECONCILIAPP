ff3611eafd4946e15959d4cdd48e09a576753808
Nicolas Garcia <nicolas.garcia@payu.com>
2021-04-26 21:23:22 -0500

    Added encuentros view

--- app/src/main/java/com/bsocial/reconciliapp/MainPage.java
+++ app/src/main/java/com/bsocial/reconciliapp/MainPage.java
@@ -2,7 +2,9 @@ package com.bsocial.reconciliapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void sendEncuentros(View view) {
+        Intent intent = new Intent(this, Encuentros.class);
+        startActivity(intent);
     }
 }
