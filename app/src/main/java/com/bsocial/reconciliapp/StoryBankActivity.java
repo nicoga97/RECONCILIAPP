dd502a6a76817a97767f3fbf204315021dbf2db7
Carlos Gomez <gemec98@gmail.com>
2021-05-23 20:29:18 -0500

    Vistas agregadas

--- app/src/main/java/com/bsocial/reconciliapp/StoryBankActivity.java
+++ app/src/main/java/com/bsocial/reconciliapp/StoryBankActivity.java
@@ -2,7 +2,9 @@ package com.bsocial.reconciliapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StoryBankActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class StoryBankActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_bank);
+    }
+
+    public void sendToMyStory(View view) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
